53ccef481a814d6201a13cbc5c36022d024a9b3b
carmencami <carmengreta3@outlook.es>
2022-07-08 19:07:29 +0000

    card

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+          <h1>${variables.name ? variables.name : "your name"} ${
+    variables.lastname ? variables.lastname : "your lastname"
+  }</h1>
+          <h2> ${variables.role ? variables.role : "Web developer"}</h2>
+          <h3>${variables.city ? variables.city : "Miami"}
+          ${variables.country ? variables.country : "USA"}</h3>
+          <ul class=${variables.socialMediaPosition}>
+          <li><a href="https://twitter.com/${
+            variables.twitter
+          }"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="https://github.com/${
+            variables.github
+          }"><i class="fab fa-github"></i></a></li>
+          <li><a href="https://linkedin.com/${
+            variables.linkedin
+          }"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="https://instagram.com/${
+            variables.instagram
+          }"><i class="fab fa-instagram"></i></a></li>
+        </ul>
         </div>
     `;
 }
@@ -61,7 +72,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
